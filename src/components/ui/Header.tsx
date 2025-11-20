18dbd51f8ba444f56aea513d5d3d90f6a0edffed
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2025-11-20 18:43:06 +0330

    .

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,16 +1,14 @@
 import { FC } from "react";
 
 const Header: FC = () => {
-    return (
-        <div className="w-full bg-blue px-16 py-20">
-            <h1 className="text-6xl text-white font-bold italic mb-4">
-                Amir Ali Mohammadi
-            </h1>
-            <h3 className="text-white text-xl">
-                Systems Programmer (Web, Network, Embedded)
-            </h3>
-        </div>
-    )
-}
+  return (
+    <div className="w-full bg-blue px-16 py-20">
+      <h1 className="text-6xl text-white font-bold italic mb-4">
+        Amir Ali Mohammadi
+      </h1>
+      <h3 className="text-white text-xl">Front End Developer</h3>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
